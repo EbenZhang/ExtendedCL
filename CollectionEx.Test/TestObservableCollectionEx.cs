2cc0873800db4da22c19a57e425103f88864ad6d
EbenZhang <zhangyingneng@gmail.com>
2015-02-20 20:50:05 +0800

    Add ObservableCollectionEx

---
+++ CollectionEx.Test/TestObservableCollectionEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CollectionEx.Test
+{
+    [TestClass]
+    public class TestObservableCollectionEx
+    {
+        [TestMethod]
+        public void TestAssignFromList()
+        {
+            var from = new List<string>()
+            {
+                "a",
+                "b",
+                "c"
+            };
+            var to = new ObservableCollection<string>();
+            to.Assign(from);
+            CollectionAssert.AreEqual(from, to);
+        }
+    }
+}
---
+++ CollectionEx/ObservableCollectionEx.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CollectionEx
+{
+    public static class ObservableCollectionEx
+    {
+        public static void Assign<T>(this ObservableCollection<T> to,  IEnumerable<T> from)
+        {
+            to.Clear();
+            foreach (var item in from)
+            {
+                to.Add(item);
+            }
+        }
+    }
+}
