c0cf0d977c3206689b2979d18c17d90a9cb2178d
EbenZhang <zhangyingneng@gmail.com>
2015-04-11 16:45:17 +0800

    MakePair

---
+++ ExtendedCL/MakePair.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ExtendedCL
+{
+    public static class MakePair
+    {
+        public static KeyValuePair<TKey, TValue> Make<TKey, TValue>(TKey k, TValue v)
+        {
+            return new KeyValuePair<TKey, TValue>(k, v);
+        }
+    }
+}
