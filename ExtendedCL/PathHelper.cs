04e72821d137a44d60b98aa452bc9dba24438886
EbenZhang <zhangyingneng@gmail.com>
2015-05-02 17:12:08 +0800

    GetCallingAssemblyPath

--- ExtendedCL/PathHelper.cs
+++ ExtendedCL/PathHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -25,6 +26,18 @@ namespace ExtendedCL
                 return;
             }
             Directory.CreateDirectory(ProcessAppDir);
+        }
+
+        public static string GetCallingAssemblyPath()
+        {
+            var codeBase = Assembly.GetCallingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            return Uri.UnescapeDataString(uri.Path);
+        }
+
+        public static string GetCallingAssemblyDir()
+        {
+            return Path.GetDirectoryName(GetCallingAssemblyPath());
         }
     }
 }
